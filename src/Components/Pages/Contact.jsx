d5d48d4ec30c158830cac855f567bd7d7cdf6dd9
arjun0071-dev <arjunchhetri202518@gmail.com>
2025-08-29 16:28:42 +0545

    changes

--- src/Components/Pages/Contact.jsx
+++ src/Components/Pages/Contact.jsx
@@ -5,29 +5,15 @@ import Map from '../PageComponents/Contact/Map'
 
 function Contact() {
 
-  const [isopen, setisopen] = useState(false)
-  return (
-    < >
-      <div className='h-lvh bg-red-900 flex items-center  justify-center'>
-        <div>
-          <button className='border-amber-600 bg-amber-900 p-[20px] rounded-4xl' onClick={() => setisopen(!isopen)}>
-          {isopen?"close":"open"}
-          </button>
-        </div>
-        <div>
-          {isopen && <div>
-            here
-          </div>
-          }
-        </div>
-      </div>
-    </>
 
-    // <div className='flex gap-24 h-fit py-9 flex-col'>
-    //    <New title="Contact"/>
-    //    <Map/>
-    //    <Touch/>
-    //   </div>
+  return (
+   
+
+    <div className='flex gap-24 h-fit py-9 flex-col'>
+       <New title="Contact"/>
+       <Map/>
+       <Touch/>
+      </div>
   )
 }
 
